89ba5f09748e61543c5c9621e4286ba761535242
zjspj <43582909+zjspj@users.noreply.github.com>
2018-11-20 08:46:20 -0800

    Add files via upload

--- manage_contig.py
+++ manage_contig.py
@@ -1,11 +1,40 @@
-x = open("iso1_onta2_canu_contigs.fasta")
-content = x.read()
-x.close()
-print(len(content))
-content.replace("N", " ")
-content = content.split()
-print(len(content))
-x = open("contig.txt", "w")
-for i in content:
-    x.write(i)
-x.close()
+import pandas as pd
+import numpy as np
+from scipy.stats import norm
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def make_list(this):
+    x = open(this)
+    contents = x.read()
+    x.close()
+    print(len(contents))
+    contents.replace("N", " ")
+    contents = contents.split()
+    content = []
+    for i in contents:
+        content.append(len(i))
+    print(len(content))
+    content.sort()
+    return content
+
+def generate_df(content):
+    df = pd.DataFrame({"first": content})
+    df = pd.value_counts(df["first"]).to_frame().reset_index()
+    df.columns = ['len', 'frequency']
+    # df["cumulative frequency"] = df["frequency"].cumsum()
+    return df
+
+
+def main():
+    x = make_list("my1.fasta")
+    # y = make_list("my2.fasta")
+    dfx = generate_df(x)
+    sns.kdeplot(data = dfx, cumulative=True)
+    plt.show()
+
+
+
+    # print(df)
+
+main()
